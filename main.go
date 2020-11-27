eb476d1cb2fc8391bc3f67d2f14ed57fcd6f34fb
hanguang <hanguang@xinxinapp.cn>
2020-11-27 14:00:31 +0800

    自定义类型和时间格式化

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 )
 
 func main() {
-
 	id := user.GetUserID()
 	userName, ok := user.GetUserName(100)
 	if ok != nil {
@@ -16,8 +15,17 @@ func main() {
 	userRandName := user.GetUserRandName()
 	fmt.Println(id, userName, userRandName)
 	userList := user.GetUserList()
+	fmt.Printf("UserList在函数外的地址%p\n", userList)
 	fmt.Printf("%#v\n", userList)
 	for _, v := range userList {
-		fmt.Println(v.UserID, v.UserName, v.Sex)
+		fmt.Println(v.UserID, v.UserName, v.Sex, v.Sex.SexToCNStr(), v.Sex.SexToENStr())
 	}
+	date := user.GetUserRegisterDate(100)
+	fmt.Printf("%#v\n", date)
+	//c := make(chan int)
+	//fmt.Printf("%#v\n", c)
+	//fmt.Printf("%T\n", c)
+	//fmt.Println(c)
+
+
 }
--- user/user.go
+++ user/user.go
@@ -2,15 +2,36 @@ package user
 
 import (
 	"errors"
+	"fmt"
 	"math/rand"
 	"time"
 )
 
+type typeSex uint8
+
 type Info struct {
 	UserID   uint
 	UserName string
-	Sex      uint8
+	Sex      typeSex
 }
+
+func (s typeSex) SexToCNStr() string {
+	sexStrMap := map[typeSex]string{
+		1: "男",
+		2: "女",
+	}
+	return sexStrMap[s]
+}
+
+func (s typeSex) SexToENStr() string {
+	sexStrMap := map[typeSex]string{
+		1: "male",
+		2: "female",
+	}
+	return sexStrMap[s]
+}
+
+
 
 func GetUserID() int {
 	return 100
@@ -28,14 +49,14 @@ func GetUserName(id int) (string, error) {
 }
 
 func GetUserRandName() string {
-	var prefixNames = [...]string{"小丸子", "小樱桃", "金刚", "抠脚", "肌肉"}
-	var suffixNames = [...]string{"姑娘", "朋友"}
+	var prefixNames = []string{"小丸子", "小樱桃", "金刚", "抠脚", "肌肉"}
+	var suffixNames = []string{"姑娘", "朋友"}
 	rand.Seed(time.Now().Unix())
 	return prefixNames[rand.Intn(len(prefixNames))] + suffixNames[rand.Intn(len(suffixNames))]
 }
 
 func GetUserList() []Info {
-	var UserList = []Info{
+	var userList = []Info{
 		{
 			100,
 			"韩",
@@ -47,5 +68,14 @@ func GetUserList() []Info {
 			2,
 		},
 	}
-	return UserList
+	fmt.Printf("UserList在函数内的地址%p\n", userList)
+	return userList
+}
+
+func GetUserRegisterDate(userId int) string {
+	var birthTimeMap = make(map[int]int64)
+	birthTimeMap[100] = 1605671940
+	birthTimeMap[200] = 1605671950
+	birthTimeMap[300] = 1605671960
+	return time.Unix(birthTimeMap[userId], 0).Format("2006-01-02 15:04:05")
 }
